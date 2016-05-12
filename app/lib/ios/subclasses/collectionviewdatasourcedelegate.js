4f73350f6acb8f7b74ce2f52d72b076f3c673c7d
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2016-05-12 16:57:01 +0200

    Add UICollectionView example

---
+++ app/lib/ios/subclasses/collectionviewdatasourcedelegate.js
@@ -0,0 +1,40 @@
+var CollectionViewDataSourceAndDelegate = Hyperloop.defineClass('CollectionViewDataSourceAndDelegate', 'NSObject', ['UICollectionViewDataSource', 'UICollectionViewDelegate', 'UICollectionViewDelegateFlowLayout']);
+
+CollectionViewDataSourceAndDelegate.addMethod({
+	selector: 'collectionView:numberOfItemsInSection:',
+	instance: true,
+	arguments: ['UICollectionView', 'long'],
+	returnType: 'long',
+	callback: function (collectionView, indexPath) {
+		if (this.numberOfCells) {
+			return this.numberOfCells(collectionView, indexPath);
+		}
+		return null;
+	}
+});
+    
+CollectionViewDataSourceAndDelegate.addMethod({
+	selector: 'collectionView:cellForItemAtIndexPath:',
+	instance: true,
+	arguments: ['UICollectionView', 'NSIndexPath'],
+	returnType: 'UICollectionViewCell',
+	callback: function (collectionView, indexPath) {
+		if (this.cellForItem) {
+			return this.cellForItem(collectionView, indexPath);
+		}
+		return null;
+	}
+});
+    
+CollectionViewDataSourceAndDelegate.addMethod({
+    selector: 'collectionView:didSelectItemAtIndexPath:',
+    instance: true,
+    arguments: ['UICollectionView', 'NSIndexPath'],
+    callback: function (collectionView, indexPath) {
+        if (this.didSelectItem) {
+            this.didSelectItem(collectionView, indexPath);
+        }
+    }
+});
+
+module.exports = CollectionViewDataSourceAndDelegate;
