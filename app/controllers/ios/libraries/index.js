0c400e1e29440dc637e27844afa09b60f116edea
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2016-05-14 22:37:14 +0200

    Move third-party libraries to sub-window

--- app/controllers/ios/libraries/index.js
+++ app/controllers/ios/libraries/index.js
@@ -1,9 +1,11 @@
+var nav;
+
 /**
  * I wrap code that executes on creation in a self-executing function just to
  * keep it organised, not to protect global scope like it would in alloy.js
  */
 (function constructor(args) {
-	$.navWin.open();
+	nav = args.nav;
 })(arguments[0] || {});
 
 function onListViewItemclick(e) {
@@ -13,5 +15,5 @@ function onListViewItemclick(e) {
 
 	// Which we use to create the controller, get the window and open it in the navigation window
 	// See lib/xp.ui.js to see how we emulate this component for Android
-	$.navWin.openWindow(Alloy.createController(controllerName, {nav: $.navWin}).getView());
+	nav.openWindow(Alloy.createController("libraries/" + controllerName).getView());
 }
