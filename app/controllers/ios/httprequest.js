80718e70c4d6653e5b9c2a78b33778a647e68402
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2016-05-14 23:31:38 +0200

    iOS: Add NSURLSession example

---
+++ app/controllers/ios/httprequest.js
@@ -0,0 +1,33 @@
+var UIApplication = require("UIKit/UIApplication");
+var NSURL = require("Foundation/NSURL");
+var NSURLSessionConfiguration = require("Foundation/NSURLSessionConfiguration");
+var NSURLSession = require("Foundation/NSURLSession");
+
+function startRequest() {
+    var URLSessionDelegate = require("subclasses/urlsessiondelegate");
+    var delegate = new URLSessionDelegate();
+    
+    delegate.didBecomeInvalidWithError = function(session, error) {
+        Ti.API.warn("Request did become invalid with error: " + error.localizedDescription);
+    };
+    
+    var urlPath = NSURL.alloc().initWithString("https://appcelerator.com");
+    var sessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration();
+    
+    // Set request headers like `User-Agent` and `Authorization`
+    sessionConfiguration.HTTPAdditionalHeaders = {"User-Agent" : "Appcelerator Hyperloop"};
+
+    var session = NSURLSession.sessionWithConfigurationDelegateDelegateQueue(sessionConfiguration, delegate, null);
+    var task = session.dataTaskWithURLCompletionHandler(urlPath, function(data, response, error) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+
+        alert("Request completed!");
+        $.btn.setTitle("Start request!");
+        $.btn.setEnabled(true);
+    });
+    
+    UIApplication.sharedApplication().networkActivityIndicatorVisible = true;
+    $.btn.setEnabled(false);
+    $.btn.setTitle("Loading ...");
+    task.resume();
+}
