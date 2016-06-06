b1ef8ac576bff7ed517caeb00c36985e983ba5f0
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2016-06-06 21:39:01 +0200

    Add destructive alert style and action-sheet

--- app/controllers/ios/alert.js
+++ app/controllers/ios/alert.js
@@ -3,23 +3,44 @@
 	var UIAlertController = require('UIKit/UIAlertController'),
 		UIAlertAction = require('UIKit/UIAlertAction'),
 		UIAlertControllerStyleAlert = require('UIKit').UIAlertControllerStyleAlert,
+		UIAlertControllerStyleActionSheet = require('UIKit').UIAlertControllerStyleActionSheet,
 		UIAlertActionStyleDefault = require('UIKit').UIAlertActionStyleDefault,
+		UIAlertActionStyleDestructive = require('UIKit').UIAlertActionStyleDestructive,
+		UIAlertActionStyleCancel = require('UIKit').UIAlertActionStyleCancel,
 		TiApp = require('Titanium/TiApp');
+		
+	function showAlertWithStyle(style) {
+		var alertController = UIAlertController.alertControllerWithTitleMessagePreferredStyle(
+			'My Title',
+			'My Message',
+			style
+		);
 
-	var alertController = UIAlertController.alertControllerWithTitleMessagePreferredStyle(
-		'My Title',
-		'My Message',
-		UIAlertControllerStyleAlert
-	);
+		var alertAction = UIAlertAction.actionWithTitleStyleHandler('OK', UIAlertActionStyleDefault, function () {
+			$.notice.setText('Clicked OK!');
+		});
+			
+		var cancelAction = UIAlertAction.actionWithTitleStyleHandler('Cancel', UIAlertActionStyleCancel, function () {
+			$.notice.setText('Clicked Cancel!');
+		});
+		
+		var destructiveAction = UIAlertAction.actionWithTitleStyleHandler('Delete', UIAlertActionStyleDestructive, function () {
+			$.notice.setText('Clicked Delete!');
+		});
 
-	var alertAction = UIAlertAction.actionWithTitleStyleHandler('OK', UIAlertActionStyleDefault, function () {
-		$.notice.setText('Clicked!');
-	});
+		alertController.addAction(alertAction);
+		alertController.addAction(destructiveAction);
+		alertController.addAction(cancelAction);
 
-	alertController.addAction(alertAction);
-
-	$.button.addEventListener('click', function () {
 		TiApp.app().showModalController(alertController, true);
+	}
+
+	$.buttonAlert.addEventListener('click', function () {
+		showAlertWithStyle(UIAlertControllerStyleAlert);
+	});
+	
+	$.buttonOptions.addEventListener('click', function () {
+		showAlertWithStyle(UIAlertControllerStyleActionSheet);
 	});
 
 })($.alert_container);
