8b822e626fb8206315c1e738aa751b909c16cd06
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2016-08-21 21:40:20 +0200

    Fix 3rd-party libraries for Android

---
+++ app/controllers/android/libraries/index.js
@@ -0,0 +1,19 @@
+var nav;
+
+/**
+ * I wrap code that executes on creation in a self-executing function just to
+ * keep it organised, not to protect global scope like it would in alloy.js
+ */
+(function constructor(args) {
+	nav = args.nav;
+})(arguments[0] || {});
+
+function onListViewItemclick(e) {
+
+	// We've set the items special itemId-property to the controller name
+	var controllerName = e.itemId;
+
+	// Which we use to create the controller, get the window and open it in the navigation window
+	// See lib/xp.ui.js to see how we emulate this component for Android
+	nav.openWindow(Alloy.createController("libraries/" + controllerName).getView());
+}
