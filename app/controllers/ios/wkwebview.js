62cc8b10ec93610c5139730e11c48f4027c618ba
hansemannn <hans.knoechel@hs-osnabrueck.de>
2017-10-24 11:13:08 +0200

    Show loader when loading WKWebView

--- app/controllers/ios/wkwebview.js
+++ app/controllers/ios/wkwebview.js
@@ -1,6 +1,8 @@
-// Arguments passed into this controller can be accessed via the `$.args` object directly or:
-var args = $.args;
+
+(function constructor(args) {
+  $.loader.show();
+})(arguments[0] || {});
 
 function onLoad(e) {
-    alert("Loaded URL: " + e.url);
+    $.loader.hide();
 }
