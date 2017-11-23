6b6ecfbc9457cc219bcdc2352eb16cfb29cb90b2
hansemannn <hans.knoechel@hs-osnabrueck.de>
2017-11-23 09:01:54 +0100

    First step of adding a Swift 4 image-picker example

--- Podfile
+++ Podfile
@@ -1,10 +1,13 @@
 install! 'cocoapods',
          :integrate_targets => false
 
-platform :ios, '8.0'
+use_frameworks!
+platform :ios, '9.0'
+
 target 'Hyperloop_Sample' do
 	pod 'JBChartView'
 	pod 'GLCalendarView', '~> 1.2'
 	pod 'Shimmer'
 	pod 'Localytics', '~> 4.4.1'
+	pod 'ImagePicker', '~> 3.0.0'
 end
---
+++ app/controllers/ios/libraries/imagePicker.js
@@ -0,0 +1,31 @@
+var ImagePickerController = require('ImagePicker/ImagePickerController');
+var ImagePickerDelegate = require('/subclasses/ImagePickerDelegate');
+var imagePicker;
+
+(function (container) {
+	// Prepare delegates
+	var imagePickerDelegate = new ImagePickerDelegate();
+
+	imagePickerDelegate.wrapperDidPress = function(imagePicker, images) {
+		Ti.API.info('wrapperDidPress');
+		Ti.API.info(images);
+	};
+	
+	imagePickerDelegate.doneButtonDidPress = function(imagePicker, images) {
+		Ti.API.info('doneButtonDidPress');
+		Ti.API.info(images);
+	};
+	
+	imagePickerDelegate.cancelButtonDidPressImagePicker = function(imagePicker) {
+		Ti.API.info('cancelButtonDidPress');
+	};
+
+	// Initialize image picker
+	imagePicker = new ImagePickerController();
+	imagePicker.delegate = imagePickerDelegate;
+	imagePicker.imageLimit = 5;	
+})($.container);
+
+function selectImage() {
+	TiApp.app().showModalController(imagePicker, true);
+}
