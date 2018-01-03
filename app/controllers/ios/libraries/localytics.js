fc6719f1503a290d899423913e2be9ff68e4d47f
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-01-03 10:17:27 +0100

    Update localytics.js

--- app/controllers/ios/libraries/localytics.js
+++ app/controllers/ios/libraries/localytics.js
@@ -1,12 +1,5 @@
 var Localytics = require('Localytics/Localytics');
 
-/**
- * Implementation of the GLCalendarView 3rd Party Library for iOS
- * using Appcelerator Hyperloop.
- *
- * Credits go to Glow-Inc (https://github.com/Glow-Inc/GLCalendarView)
- * for their great open source calendar!
- */
 (function (container) {
 	// Initialize SDK
 	Localytics.autoIntegrateLaunchOptions(Alloy.CFG.services.localytics.apiKey, null);	
