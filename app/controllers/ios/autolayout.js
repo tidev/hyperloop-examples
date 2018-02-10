9d96637ac27189d83695920646462106ccb07e2d
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-02-10 22:54:22 +0100

    Update autolayout example, use 7.0.2

--- app/controllers/ios/autolayout.js
+++ app/controllers/ios/autolayout.js
@@ -1,14 +1,12 @@
-// hyperloop code ... Go!
+var UILabel = require('UIKit/UILabel'),
+	UIColor = require('UIKit/UIColor'),
+	UIScreen = require('UIKit/UIScreen'),
+	UIView = require('UIKit/UIView'),
+	CGRectMake = require('CoreGraphics/CoreGraphics').CGRectMake,
+	UIKit = require('UIKit/UIKit'),
+	NSLayoutConstraint = require('UIKit/NSLayoutConstraint');
+
 (function (container) {
-
-	var UILabel = require('UIKit/UILabel'),
-		UIColor = require('UIKit/UIColor'),
-		UIScreen = require('UIKit/UIScreen'),
-		UIView = require('UIKit/UIView'),
-		CGRectMake = require('CoreGraphics/CoreGraphics').CGRectMake,
-		UIKit = require('UIKit/UIKit'),
-		NSLayoutConstraint = require('UIKit/NSLayoutConstraint');
-
 	var label = new UILabel();
 	label.translatesAutoresizingMaskIntoConstraints = false;
 	label.setText('Hello World');
@@ -20,51 +18,64 @@
 	view.setBackgroundColor(UIColor.yellowColor);
 	view.addSubview(label);
 
-	var heightConstraint = NSLayoutConstraint.constraintWithItemAttributeRelatedByToItemAttributeMultiplierConstant(
-		label,
-		UIKit.NSLayoutAttributeHeight,
-		UIKit.NSLayoutRelationEqual,
-		null,
-		UIKit.NSLayoutAttributeNotAnAttribute,
-		1,
-		100
-	);
+	var heightConstraint = {
+		item: label,
+		attribute: UIKit.NSLayoutAttributeHeight,
+		relatedBy: UIKit.NSLayoutRelationEqual,
+		toItem: null,
+		toAttribute: UIKit.NSLayoutAttributeNotAnAttribute,
+		multiplier: 1.0,
+		constant: 100.0
+	};
 
-	var widthConstraint = NSLayoutConstraint.constraintWithItemAttributeRelatedByToItemAttributeMultiplierConstant(
-		label,
-		UIKit.NSLayoutAttributeWidth,
-		UIKit.NSLayoutRelationEqual,
-		null,
-		UIKit.NSLayoutAttributeNotAnAttribute,
-		1,
-		200
-	);
+	var widthConstraint = {
+		item: label,
+		attribute: UIKit.NSLayoutAttributeWidth,
+		relatedBy: UIKit.NSLayoutRelationEqual,
+		toItem: null,
+		toAttribute: UIKit.NSLayoutAttributeNotAnAttribute,
+		multiplier: 1.0,
+		constant: 200.0
+	};
 
-	var horizontalConstraint = NSLayoutConstraint.constraintWithItemAttributeRelatedByToItemAttributeMultiplierConstant(
-		label,
-		UIKit.NSLayoutAttributeCenterX,
-		UIKit.NSLayoutRelationEqual,
-		view,
-		UIKit.NSLayoutAttributeCenterX,
-		1,
-		0
-	);
+	var horizontalConstraint = {
+		item: label,
+		attribute: UIKit.NSLayoutAttributeCenterX,
+		relatedBy: UIKit.NSLayoutRelationEqual,
+		toItem: view,
+		toAttribute: UIKit.NSLayoutAttributeCenterX,
+		multiplier: 1.0,
+		constant: 0.0
+	};
 
-	var verticalConstraint = NSLayoutConstraint.constraintWithItemAttributeRelatedByToItemAttributeMultiplierConstant(
-		label,
-		UIKit.NSLayoutAttributeCenterY,
-		UIKit.NSLayoutRelationEqual,
-		view,
-		UIKit.NSLayoutAttributeCenterY,
-		1,
-		0
-	);
+	var verticalConstraint = {
+		item: label,
+		attribute: UIKit.NSLayoutAttributeCenterY,
+		relatedBy: UIKit.NSLayoutRelationEqual,
+		toItem: view,
+		toAttribute: UIKit.NSLayoutAttributeCenterY,
+		multiplier: 1.0,
+		constant: 0.0
+	};
 
-	label.addConstraint(heightConstraint);
-	label.addConstraint(widthConstraint);
-	view.addConstraint(horizontalConstraint);
-	view.addConstraint(verticalConstraint);
+	addConstraint(label, heightConstraint);
+	addConstraint(label, widthConstraint);
+	addConstraint(view, horizontalConstraint);
+	addConstraint(view, verticalConstraint);
 
 	container.add(view);
 
 })($.autolayout_container);
+
+function addConstraint(element, options) {
+	var constraint = NSLayoutConstraint.constraintWithItemAttributeRelatedByToItemAttributeMultiplierConstant(
+		options.item,
+		options.attribute,
+		options.relatedBy,
+		options.toItem,
+		options.toAttribute,
+		options.multiplier,
+		options.constant
+	);
+	element.addConstraint(constraint);
+}
