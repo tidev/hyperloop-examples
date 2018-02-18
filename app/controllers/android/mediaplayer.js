d983520db7f10d2ad5209fce09b9f560621aa1f3
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-02-18 12:40:06 +0100

    Add Android MediaPlayer example

---
+++ app/controllers/android/mediaplayer.js
@@ -0,0 +1,34 @@
+
+var Activity = require('android.app.Activity');
+var AudioManager = require('android.media.AudioManager');
+var MediaPlayer = require('android.media.MediaPlayer');
+var Uri = require('android.net.Uri');
+var activity = new Activity(Ti.Android.currentActivity);
+var context = activity.getApplicationContext();
+var mMediaPlayer;
+
+(function(container) {
+	var contentUri = Uri.parse('android.resource://' + activity.getPackageName() + '/raw/audio');
+	
+	mMediaPlayer = new MediaPlayer();
+	mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener({
+		onCompletion: function (mediaPlayer) {
+			Ti.API.info('MediaPlayer playback completed');
+		}
+	}));
+	mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+	mMediaPlayer.setDataSource(context, contentUri);
+	mMediaPlayer.prepare();
+})($.window);
+
+function startMedia() {
+	mMediaPlayer.start();
+}
+
+function stopMedia() {
+	mMediaPlayer.pause();
+	
+	// NOTE: You can also stop it, but then you have to prepare() it again as well
+	// mMediaPlayer.stop();
+	// mMediaPlayer.prepare();
+}
