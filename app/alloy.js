4c2f35295510fa607e05aa7e5f62501b5e4c4e38
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-08-19 11:14:58 +0200

    feat: add UIApplicationDelegate API's

--- app/alloy.js
+++ app/alloy.js
@@ -1,13 +1,58 @@
-/* global Alloy, ENV_PROD */
 
-// The contents of this file will be executed before any of
-// your view controllers are ever executed, including the index.
-// You have access to all functionality on the `Alloy` namespace.
-//
-// This is a great place to do any initialization for your app
-// or create any global variables/functions that you'd like to
-// make available throughout your app. You can easily make things
-// accessible globally by attaching them to the `Alloy.Globals`
-// object. For example:
-//
-// Alloy.Globals.someGlobalFunction = () => { ... };
+// Register UIApplicationDelegate API's for iOS
+if (OS_IOS) {
+    registerApplicationDelegate();
+}
+
+function registerApplicationDelegate() {
+    const TiApp = require('Titanium/TiApp');
+
+    // Create a new class to handle the delegate
+    var ApplicationDelegate = Hyperloop.defineClass('TiAppApplicationDelegate', 'NSObject', 'UIApplicationDelegate');
+
+    // Add the selector to handle the result
+    ApplicationDelegate.addMethod({
+        selector: 'application:didFinishLaunchingWithOptions:',
+        instance: true,
+        returnType: 'BOOL',
+        arguments: [
+            'UIApplication',
+            'NSDictionary'
+        ],
+        callback: function(application, options) {
+            if (this.didFinishLaunchingWithOptions) {
+                return this.didFinishLaunchingWithOptions(application, options);
+            }
+            return true;
+        }
+    });
+
+    // Add the selector to handle the result
+    ApplicationDelegate.addMethod({
+        selector: 'applicationDidEnterBackground:',
+        instance: true,
+        arguments: [
+            'UIApplication'
+        ],
+        callback: function(application) {
+            if (this.applicationDidEnterBackground) {
+                this.applicationDidEnterBackground(application);
+            }
+        }
+    });
+
+    // Instantiate the delegate subclass
+    var applicationDelegate = new ApplicationDelegate();
+
+    // Called when the application finished launching. Initialize SDK's here for example
+    applicationDelegate.didFinishLaunchingWithOptions = function (application, options) {
+        Ti.API.warn('didFinishLaunchingWithOptions: called!');
+        return true
+    };
+
+    applicationDelegate.applicationDidEnterBackground = function (application) {
+        Ti.API.warn('applicationDidEnterBackground: called!');
+    };
+
+    TiApp.app().registerApplicationDelegate(ApplicationDelegate);
+}
