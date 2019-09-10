c85745fbaeb589aad945df24e77529fce7b30ada
Jan Vennemann <jan.vennemann@gmx.net>
2019-09-10 15:01:26 +0200

    fix(ios): remove deprecated custom src examples

--- .appc.js
+++ .appc.js
@@ -16,39 +16,20 @@ module.exports = {
           GCC_PREPROCESSOR_DEFINITIONS: 'foo=bar'
         },
         /**
-         * this sample doesn't use StoreKit but this demonstrates
+         * this sample doesn't use SceneKit but this demonstrates
          * how you can bring in frameworks explicitly. Hyperloop
          * will automatically determine the required frameworks
          * but in case you want to force a specific version, you can
          * include it here
          */
         frameworks: [
-          'StoreKit'
+          'SceneKit'
         ],
 
         scripts: [{
           name: 'My script phase',
           shellScript: '${APPC_PROJECT_DIR}/run-script.sh'
         }]
-      },
-      /**
-       * optionally, you can bring in third-party or first-party libraries,
-       * source code, resources etc. by including them here. The 'key' is the
-       * name of the package that will be used in the require (if code).
-       * the values can either be an Array or String value to the directory
-       * where the files are located
-       *
-       * **WARNING**: This functionality has been deprecated in Hyperloop 3.0.0
-       * and will be removed in future versions of Hyperloop. Use Cocoapods or 
-       * place your frameworks in platform/ios instead to be properly handled.
-       */
-      thirdparty: {
-        'MyFramework': {
-          // these can be an array or string
-          source: ['src'],
-          header: 'src',
-          resource: 'src'
-        }
       }
     }
   }
--- src/MySwift.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-
-public class MySwiftView : UIImageView {
-	convenience init () {
-		self.init(image: UIImage.init(named:"swift.png"))
-	}
-}
