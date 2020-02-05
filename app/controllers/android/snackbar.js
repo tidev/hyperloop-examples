00f4c1d20747ee5adfacbf2d0cb26dba644c8a43
Joshua Quick <jquick@axway.com>
2020-02-04 19:04:05 -0800

    chore(android): resolved snackbar constant access
    - Google move constant from Snackbar class to its super class.
    - Hyperloop does not currently duplicate inherited constants to child classes, so we must access constant from base class directly.

--- app/controllers/android/snackbar.js
+++ app/controllers/android/snackbar.js
@@ -1,8 +1,9 @@
+import BaseTransientBottomBar from 'com.google.android.material.snackbar.BaseTransientBottomBar';
 import Snackbar from 'com.google.android.material.snackbar.Snackbar';
 
 (function (container) {
 	$.button.addEventListener('click', () => {
-		const snack = Snackbar.make(container, 'Hello world!', Snackbar.LENGTH_LONG);
+		const snack = Snackbar.make(container, 'Hello world!', BaseTransientBottomBar.LENGTH_LONG);
 		snack.show();
 	});
 })($.alert_container);
